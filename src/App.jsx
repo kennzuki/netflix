6e26bb6b7e3ecc83bdfe44bafceac4feab25c727
kennzuki <kingsyken@gmail.com>
2024-11-04 16:06:37 +0300

    folder structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <div className="">
+      <Home/>
+    </div>
   );
 }
 
